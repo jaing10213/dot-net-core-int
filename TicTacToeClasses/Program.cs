2dbd676d4e39f9e79d350fcd01a523dac26cdb65
Gaurav Jain <gaurav.jain@vertiv.com>
2022-05-18 14:24:28 -0400

    added tic-toe-project

---
+++ TicTacToeClasses/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicTacToeClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Design a game of Tic - Tac - Toe
+            //    What all classes are needed
+            //    What all properties and methods
+            //    Relationship between the classes
+            //    Create UML diagram of the same
+
+        }
+    }
+}
